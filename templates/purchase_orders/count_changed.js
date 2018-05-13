97afd42d6c3945f8c91185542eb924fcf7a4e6f8
Anil Dhurjaty <adhurjaty@novacoast.com>
2018-05-12 17:04:59 -0700

    disappears breakdown at 0 items

--- templates/purchase_orders/count_changed.js
+++ templates/purchase_orders/count_changed.js
@@ -1,1 +1,6 @@
-$('#order-category-breakdown').html('<%= partial("partials/horizontal_percentage_chart.html", {categoryDetails: categoryDetails, title: title}) %>  ');
+<%= if (categoryDetails.Total == 0.0) { %>
+    var content = "";
+<% } else { %>
+    var content = '<%= partial("partials/horizontal_percentage_chart.html", {categoryDetails: categoryDetails, title: title}) %>';
+<% } %>
+$('#order-category-breakdown').html(content);
