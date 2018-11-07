24a153fac009d9d2566ecb840f39196372c4edbe
Anil <adhurjaty@gmail.com>
2018-11-07 15:00:03 -0800

    cleans up purchase_order_form

--- assets/js/orders/purchase_order_form.js
+++ assets/js/orders/purchase_order_form.js
@@ -1,58 +1,75 @@
 import { initOrderItemsArea } from './_order_item_details';
 import { datepicker, daterange } from '../_datepicker';
 
-var _vendorItemsMap;
 
 $(() => {
-    _vendorItemsMap = JSON.parse($('#vendor-items-map').attr('data'));
+    var vendorItemsMap = JSON.parse($('#vendor-items-map').attr('data'));
 
+    setDateRange();
+    setDropdown(vendorItemsMap);
+    setFormOnSubmit();
+    setDateCheckbox();
+});
+
+function setDateRange() {
     var $inputs = $('#new-order-date, #new-received-date');
     daterange($inputs);
+}
 
-    if($('#new-order-vendor option:selected').val()) {
+function setDropdown(vendorItemsMap) {
+    var dropdownId = '#new-order-vendor';
+    if($(`${dropdownId} option:selected`).val()) {
         var items = JSON.parse($('#vendor-items-table').attr('data'));
         initOrderItemsArea(items)
     }
-    $('#new-order-vendor').change(function(d) {
+    $(dropdownId).change(function(d) {
         // remove none option
-        $('#new-order-vendor option[value=""]').remove();
+        $(`${dropdownId} option[value=""]`).remove();
+
         var id = $(this).val();
-        
-        var items = _vendorItemsMap[id];
+        var items = vendorItemsMap[id];
 
         // initialize grid and breakdown
         initOrderItemsArea(items);
     });
+}
 
+function setFormOnSubmit() {
     $('#purchase-order-form-submit').closest('form').submit(function(event) {
-        if($('#new-order-vendor option:selected').val().length == 0) {
+        if(!isValidOptionSelected()) {
             event.preventDefault();
             showError('must select a vendor');
             return;
         }
 
-        var data = JSON.parse($('#vendor-items-table').attr('data'));
-        data = data.filter((x) => x.count > 0);
+        var data = getDataFromTable();
         if(data.length == 0) {
             event.preventDefault();
             showError('must order at least one item');
             return;
         }
         
-        if(!$('#received-order-checkbox').is(':checked')) {
-            $('#received-date-input').remove();
-        }
+        removeUncheckedRecDate();
+        
         sendOrderItems(data)
     });
+}
 
-    $('#received-order-checkbox').change(function() {
-        if(this.checked) {
-            $('#received-date-input').show();
-        } else {
-            $('#received-date-input').hide();
-        }
-    });
-});
+function isValidOptionSelected() {
+    return $('#new-order-vendor option:selected').val().length > 0;
+}
+
+function getDataFromTable() {
+    return JSON.parse($('#vendor-items-table')
+               .attr('data'))
+               .filter((x) => x.count > 0);
+}
+
+function removeUncheckedRecDate() {
+    if(!$('#received-order-checkbox').is(':checked')) {
+        $('#received-date-input').remove();
+    }
+}
 
 function sendOrderItems(data) {
     var $input = $('form input[name="Items"]');
@@ -62,4 +79,14 @@ function sendOrderItems(data) {
 function showError(msg) {
     $('#form-errors').text(msg);
     $('#form-errors').show();
+}
+
+function setDateCheckbox() {
+    $('#received-order-checkbox').change(function() {
+        if(this.checked) {
+            $('#received-date-input').show();
+        } else {
+            $('#received-date-input').hide();
+        }
+    });
 }
