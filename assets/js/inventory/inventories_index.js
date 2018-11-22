2b343f9185fef6f31a9ef07f610de68426b88709
Anil <adhurjaty@gmail.com>
2018-11-22 09:52:22 -0500

    Sets date switching in inventory

--- assets/js/inventory/inventories_index.js
+++ assets/js/inventory/inventories_index.js
@@ -2,67 +2,80 @@ import { replaceUrlId, parseModelJSON, formatSlashDate } from '../helpers/_helpe
 import { sendUpdate } from '../helpers/index_helpers';
 import { CollapsibleDatagrid } from '../datagrid/_collapsible_datagrid';
 
+
 $(() => {
-    var $container = $('#datagrid-holder');
-    var editInventoryPath = $container.attr('data-url');
+    var invList = $('#date-list');
+    invList.find('li').first().addClass('active');
 
-    var columnObjects = [
-        {
-            name: 'id',
-            hidden: true,
-            column_func: (inventory) => {
-                return inventory.id;
-            }
-        },
-        {
-            name: 'date',
-            header: 'Date',
-            editable: true,
-            data_type: 'date',
-            column_func: (inventory) => {
-                return formatSlashDate(inventory.time);
-            }
-        },
-        {
-            name: 'dropdown',
-            column_func: ((editInventoryPath) => {
-                return (inventory) => {
-                    return `<div class="dropdown show">
-                            <button type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                ...
-                            </button>
-                            <div class="dropdown-menu">
-                                <a href="${replaceUrlId(editInventoryPath, inventory.id)}" class="dropdown-item">Edit</a>
-                            </div>
-                        </div>`
-                }
-            })(editInventoryPath)
-        }
-    ];
+    setClickInventory();
+    setSelectedInventory();
 
-    var dataStr = $container.attr('data');
-    var data = parseModelJSON(dataStr) || [];
+    // var $container = $('#datagrid-holder');
+    // var editInventoryPath = $container.attr('data-url');
 
-    var datagrid = new CollapsibleDatagrid(data, columnObjects, getHiddenRow, sendDatagridUpdate);
-    $container.html(datagrid.getTable());
+    // var columnObjects = [
+    //     {
+    //         name: 'id',
+    //         hidden: true,
+    //         column_func: (inventory) => {
+    //             return inventory.id;
+    //         }
+    //     },
+    //     {
+    //         name: 'date',
+    //         header: 'Date',
+    //         editable: true,
+    //         data_type: 'date',
+    //         column_func: (inventory) => {
+    //             return formatSlashDate(inventory.time);
+    //         }
+    //     },
+    //     {
+    //         name: 'dropdown',
+    //         column_func: ((editInventoryPath) => {
+    //             return (inventory) => {
+    //                 return `<div class="dropdown show">
+    //                         <button type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    //                             ...
+    //                         </button>
+    //                         <div class="dropdown-menu">
+    //                             <a href="${replaceUrlId(editInventoryPath, inventory.id)}" class="dropdown-item">Edit</a>
+    //                         </div>
+    //                     </div>`
+    //             }
+    //         })(editInventoryPath)
+    //     }
+    // ];
+
+    // var dataStr = $container.attr('data');
+    // var data = parseModelJSON(dataStr) || [];
+
+    // var datagrid = new CollapsibleDatagrid(data, columnObjects, getHiddenRow, sendDatagridUpdate);
+    // $container.html(datagrid.getTable());
 });
 
-// TODO: repeated code from purchase_orders_datagrid.js - refactor
-function sendDatagridUpdate(updateObj) {
-    sendUpdate($('#update-inventory-form'), updateObj);
+function setClickInventory() {
+    var listItems = $('#date-list').find('li');
+    listItems.each((i, el) => {
+        var $el = $(el);
+        $el.click(() => {
+            listItems.removeClass('active');
+            $el.addClass('active');
+            setSelectedInventory();
+        });
+    });
 }
 
-function getHiddenRow(inventory) {
-    return `<tr class="items-list">
-            <td colspan="100">
-                <table>
-                    ${inventory.Items.map((item) => {
-                        return `<tr>
-                            <td>${item.name}</td>
-                            <td>${item.count}</td>
-                        </tr>`
-                    }).join('\n')}
-                </table>
-            </td>
-        </tr>`
+function setSelectedInventory() {
+    var $form = $('#inventory-form');
+    var invList = $('#date-list');
+    var selectedInventory = JSON.parse(invList.find('li.active').attr('data'));
+    $form.find('input[name="Date"]').val(formatSlashDate(selectedInventory.time));
+
 }
+
+// TODO: repeated code from purchase_orders_datagrid.js - refactor
+// function sendDatagridUpdate(updateObj) {
+//     sendUpdate($('#update-inventory-form'), updateObj);
+// }
+
