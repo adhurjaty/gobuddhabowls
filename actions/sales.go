b890aa07d23afb4c5c5f180be3d48ad76fee6fb2
Anil <adhurjaty@gmail.com>
2019-01-21 10:11:59 -0800

    shows basic sales index page

--- actions/sales.go
+++ actions/sales.go
@@ -1,10 +1,23 @@
 package actions
 
 import (
+	"buddhabowls/presentation"
 	"github.com/gobuffalo/buffalo"
+	"github.com/gobuffalo/pop"
+	"github.com/pkg/errors"
 )
 
 func ListSales(c buffalo.Context) error {
+	tx, ok := c.Value("tx").(*pop.Connection)
+	if !ok {
+		return errors.WithStack(errors.New("no transaction found"))
+	}
+
+	presenter := presentation.NewPresenter(tx)
+	_, _, err := setPeriodSelector(c, presenter)
+	if err != nil {
+		return errors.WithStack(err)
+	}
 
 	return c.Render(200, r.HTML("sales/index"))
 }
