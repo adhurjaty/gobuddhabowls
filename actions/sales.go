9d75809be68e5c1830f81fbb9026f9db67bcb07a
Anil <adhurjaty@gmail.com>
2019-01-22 16:45:17 -0800

    loads square transaction data

--- actions/sales.go
+++ actions/sales.go
@@ -1,11 +1,66 @@
 package actions
 
 import (
+	"buddhabowls/helpers"
 	"buddhabowls/presentation"
+	"encoding/json"
+	"fmt"
 	"github.com/gobuffalo/buffalo"
 	"github.com/gobuffalo/pop"
 	"github.com/pkg/errors"
+	"io/ioutil"
+	"net/http"
+	"time"
 )
+
+type transactionResult struct {
+	Errors       []apiError    `json:"errors,omitempty"`
+	Transactions []transaction `json:"transactions"`
+	Cursor       string        `json:"cursor,omitempty"`
+}
+
+type apiError struct {
+	Field  string `json:"field"`
+	Detail string `json:"detail`
+}
+
+type transaction struct {
+	TransactionTime time.Time `json:"created_at"`
+	Tenders         []tender  `json:"tenders,omitempty"`
+	Refunds         []refund  `json:"refunds,omitempty"`
+}
+
+type tender struct {
+	Amount money `json:"amount_money"`
+	Tip    money `json:"tip_money"`
+	Fee    money `json:"processing_fee_money"`
+}
+
+type money struct {
+	Amount   int    `json:"amount"`
+	Currency string `json:"currency"`
+}
+
+type refund struct {
+	Amount money `json:"amount_money"`
+	Fee    money `json:"processing_fee_money"`
+}
+
+type SquareSale struct {
+	TransactionTime time.Time
+	Amount          float64
+	Tip             float64
+	Fee             float64
+}
+
+type SquareSales []SquareSale
+
+func (s SquareSales) String() string {
+	sj, _ := json.Marshal(s)
+	return string(sj)
+}
+
+const squareURLBase = "https://connect.squareup.com/v2/locations/"
 
 func ListSales(c buffalo.Context) error {
 	tx, ok := c.Value("tx").(*pop.Connection)
@@ -14,13 +69,108 @@ func ListSales(c buffalo.Context) error {
 	}
 
 	presenter := presentation.NewPresenter(tx)
-	_, _, err := setPeriodSelector(c, presenter)
+	startTime, endTime, err := setPeriodSelector(c, presenter)
 	if err != nil {
 		return errors.WithStack(err)
 	}
 
-	locationID := "69VJ030ANYAGV"
-	c.Set("locationID", locationID)
-	c.Set("squareToken", "sq0atp-Zo5ieRMqg6UpcSsAzSLEJQ")
+	sales, err := getSquareSales(startTime, endTime)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	c.Set("sales", *sales)
+
 	return c.Render(200, r.HTML("sales/index"))
+}
+
+func getTransactionURL(locationID string, startTime time.Time, endTime time.Time) string {
+	startParam := helpers.RFC3339Date(startTime)
+	endParam := helpers.RFC3339Date(endTime)
+
+	return fmt.Sprintf("%s%s/transactions?begin_time=%s&end_time=%s",
+		squareURLBase, locationID, startParam, endParam)
+}
+
+func addCursorToURL(url string, cursor string) string {
+	if cursor == "" {
+		return url
+	}
+	return url + "&cursor=" + cursor
+}
+
+func sendGetRequest(url string, token string) (*http.Response, error) {
+	client := http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Authorization", "Bearer "+token)
+	return client.Do(req)
+}
+
+func getSquareSales(startTime time.Time, endTime time.Time) (*SquareSales, error) {
+
+	locationID := "69VJ030ANYAGV"
+	// remember to change the token when pushing to remote
+	squareToken := "sq0atp-Zo5ieRMqg6UpcSsAzSLEJQ"
+	cursor := ""
+	allTransactions := &[]transaction{}
+
+	for {
+		transactionURL := getTransactionURL(locationID, startTime, endTime)
+		transactionURL = addCursorToURL(transactionURL, cursor)
+
+		resp, err := sendGetRequest(transactionURL, squareToken)
+		if err != nil {
+			return nil, err
+		}
+		jsonBytes, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		results := &transactionResult{}
+		json.Unmarshal(jsonBytes, results)
+		if results.Errors != nil {
+			return nil, errors.New(fmt.Sprintf("%s: %s",
+				results.Errors[0].Field, results.Errors[0].Detail))
+		}
+
+		*allTransactions = append(*allTransactions, results.Transactions...)
+
+		cursor = results.Cursor
+		if cursor == "" {
+			break
+		}
+	}
+
+	allSales := toSales(allTransactions)
+
+	return allSales, nil
+}
+
+func toSales(transactions *[]transaction) *SquareSales {
+	sales := &SquareSales{}
+	for _, transaction := range *transactions {
+		sale := SquareSale{
+			TransactionTime: transaction.TransactionTime,
+		}
+
+		for _, tender := range transaction.Tenders {
+			sale.Amount += float64(tender.Amount.Amount) / 100.0
+			sale.Fee += float64(tender.Fee.Amount) / 100.0
+			sale.Tip += float64(tender.Tip.Amount) / 100.0
+		}
+
+		for _, refund := range transaction.Refunds {
+			sale.Amount -= float64(refund.Amount.Amount) / 100.0
+			sale.Fee += float64(refund.Fee.Amount) / 100.
+		}
+
+		*sales = append(*sales, sale)
+	}
+
+	return sales
 }
--- assets/js/sales/sales_index.js
+++ assets/js/sales/sales_index.js
@@ -1,50 +1,23 @@
-import { transactionURL } from "../helpers/_square_helper";
-import { showError } from "../helpers/index_helpers";
+import { parseModelJSON } from "../helpers/_helpers";
 
-var _sales = []
+// var _columnInfo = [
+//     {
+//         header: 'Name',
+//         get_column: (item) => {
+//             item.name;
+//         }
+//     },
+//     {
+//         header: ''
+//     }
+// ]
 
 $(() => {
-    getSquareData();
+    initDatagrid();
 });
 
 
-function getSquareData() {
-    var infoContainer = $('#square-info');
-    var token = infoContainer.attr('square-token');
-    var locationID = infoContainer.attr('square-location');
-
-    var dateStrs = getDateRange();
-    var startTime = new Date(dateStrs[0]);
-    var endTime = new Date(dateStrs[1]);
-    var url = transactionURL(locationID, startTime, endTime);
-
-    getTransactionAjax(url, token);
-}
-
-function getDateRange() {
-
-}
-
-function getTransactionAjax(url, token) {
-    $.ajax(url, {
-        method: 'GET',
-        headers: {Authorization: `Bearer: ${token}`},
-        error: (xhr, status, err) => {
-            showError(err);
-        },
-        success: (data, status, xhr) => {
-            populateDatagrid(data);
-        },
-        complete: () => {
-            removeLoader();
-        }
-    });
-}
-
-function populateDatagrid(data) {
-
-}
-
-function removeLoader() {
-    $('#loading-container').remove();
+function initDatagrid() {
+    var sales = parseModelJSON($('#sales-datagrid').attr('data'));
+    debugger;
 }
