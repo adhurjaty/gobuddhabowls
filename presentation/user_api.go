5da1599eb883d2459ea32373e0d1e232a4822eda
Anil <adhurjaty@gmail.com>
2019-01-23 14:58:17 -0800

    creates user presentation and logic files

---
+++ presentation/user_api.go
@@ -0,0 +1,44 @@
+package presentation
+
+import (
+	"buddhabowls/models"
+	"encoding/json"
+	"github.com/gobuffalo/uuid"
+)
+
+type UserAPI struct {
+	ID             string `json:"id"`
+	Email          string `json:"email"`
+	SquareLocation string `json:"square_location"`
+	SquareToken    string `json:"square_token"`
+	Timezone       string `json:"timezone"`
+}
+
+func (u UserAPI) String() string {
+	ju, _ := json.Marshal(u)
+	return string(ju)
+}
+
+func NewUserAPI(user *models.User) UserAPI {
+	return UserAPI{
+		Email:          user.Email,
+		SquareLocation: user.SquareLocation.String,
+		SquareToken:    user.SquareToken.String,
+		Timezone:       user.Timezone,
+	}
+}
+
+func ConvertToModelUser(userAPI *UserAPI) (*models.User, error) {
+	id, err := uuid.FromString(userAPI.ID)
+	if err != nil {
+		id = uuid.UUID{}
+	}
+
+	return &models.User{
+		ID:             id,
+		Email:          userAPI.Email,
+		SquareLocation: models.StringToNullString(userAPI.SquareLocation),
+		SquareToken:    models.StringToNullString(userAPI.SquareToken),
+		Timezone:       userAPI.Timezone,
+	}, nil
+}
