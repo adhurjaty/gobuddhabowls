77ffc1b00df6db5ca6bd9928d53887ccb3cd3f66
Anil <adhurjaty@gmail.com>
2019-01-23 16:33:47 -0800

    removes square js helper

--- assets/js/helpers/_square_helper.js
+++
@@ -1,8 +0,0 @@
-import { rfc3339 } from "./_helpers";
-
-export function transactionURL(locationID, startTime, endTime) {
-    var formattedStart = rfc3339(startTime);
-    var formattedEnd = rfc3339(endTime);
-    return `https://connect.squareup.com/v2/locations/${locationID}`
-        + `/transactions?begin_time=${formattedStart}&end_time=${formattedEnd}`;
-}
