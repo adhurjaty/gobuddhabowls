30e9ab033011d5dd0300737f2aa3fbfba0e27b9a
Anil Dhurjaty <adhurjaty@gmail.com>
2019-06-16 13:26:46 -0700

    adds functions for prep items form

--- assets/js/prep_items/prep_items_form.js
+++ assets/js/prep_items/prep_items_form.js
@@ -1,3 +1,28 @@
 $(() => {
-
+    setInventoryItemCheck();
 });
+
+function setOnSubmit() {
+    var form = $('button[role="submit"]').closest('form');
+    form.submit(removeFields);
+}
+
+function removeFields() {
+    var invCheck = $('#inventory-item-check');
+    if(invCheck.is(':checked')) {
+        $('#recipes-selector').remove();
+    } else {
+        $('#inventory-items-selector').remove();
+    }
+    invCheck.remove();
+}
+
+function setInventoryItemCheck() {
+    var checkbox = $('inventory-item-check');
+    checkbox.change(toggleShowRecipe);
+}
+
+function toggleShowRecipe() {
+    $('#recipes-selector').toggle();
+    $('#inventory-items-selector').toggle();
+}
