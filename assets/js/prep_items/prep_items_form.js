d9efb27a1ade0df984552657cf3153fcb0a7408b
Anil Dhurjaty <adhurjaty@gmail.com>
2019-07-02 18:04:03 -0700

    Changes recipe unit to readonly

--- assets/js/prep_items/prep_items_form.js
+++ assets/js/prep_items/prep_items_form.js
@@ -1,3 +1,5 @@
+import { parseModelJSON } from "../helpers/_helpers";
+
 var _recipeSelector;
 var _invItemSelector;
 
@@ -8,6 +10,7 @@ $(() => {
 
     setOnSubmit();
     setInventoryItemCheck();
+    setChangeRecipeUnit();
 });
 
 function setOnSubmit() {
@@ -27,10 +30,34 @@ function removeFields() {
 
 function setInventoryItemCheck() {
     var checkbox = $('#inventory-item-check');
-    checkbox.change(toggleShowRecipe);
+    checkbox.change((e) => {
+        toggleShowRecipe();
+        if($(e.target).is(':checked'))
+            $('select[name="InventoryItemID"]').trigger('change');
+        else
+            $('select[name="BatchRecipeID"]').trigger('change');
+    });
 }
 
 function toggleShowRecipe() {
     _recipeSelector.toggle();
     _invItemSelector.toggle();
 }
+
+function setChangeRecipeUnit() {
+    $('select[name="BatchRecipeID"]').change((e) => {
+        setOtherUnitUI($(e.target), 'Recipe Unit', item => item.recipe_unit);
+    });
+    $('select[name="InventoryItemID"]').change((e) => {
+        setOtherUnitUI($(e.target), "Inventory Count Unit", item => item.count_unit);
+    });
+}
+
+function setOtherUnitUI(el, label, getUnit) {
+    var id = el.find('option:selected').val();
+    var recipes = parseModelJSON(el.attr('data'));
+    var item = recipes.find(x => x.id == id);
+    $('#other-unit-label').html(label);
+    $('#other-unit').html(getUnit(item));
+}
+
